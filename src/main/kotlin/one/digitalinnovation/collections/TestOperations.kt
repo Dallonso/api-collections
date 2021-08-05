d1f9d7d71b53ac72f363ed720e83ef99b412aaf5
Dallonso <dallonso@yahoo.com.br>
2021-08-05 20:13:41 -0300

    Add Array filter, any, find

--- src/main/kotlin/one/digitalinnovation/collections/TestOperations.kt
+++ src/main/kotlin/one/digitalinnovation/collections/TestOperations.kt
@@ -15,4 +15,18 @@ fun main(){
     println("---------------------")
     val wageFilter = wages.filter { it > 2500.0 }
     wageFilter.forEach { println(it) }
+
+    //Função Count: Função conta numero de ites. No exemplo foi especificado um range para tal
+    println("---------------------")
+    println(wages.count{it in 2000.0..5000.0})
+
+    //Função Find: Função  busca itens. No exemplo foi especificado um item para ser verificado
+    println("---------------------")
+    println(wages.find{it == 2250.0})
+    println(wages.find{it == 500.0})
+
+    //Função Any: Função busca itens e retorna ture ou false. No exemplo foi especificado um item para ser verificado
+    println("---------------------")
+    println(wages.any{it == 1000.0})
+    println(wages.any{it == 500.0})
 }
